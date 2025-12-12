42794f836edb58cc55d801ec3be97d5b4e5d721e
makai <makai.moodybroen@maine.edu>
2025-12-12 15:30:34 +0000

    proxy manager implemented

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const comments = [];
 
 // Middleware 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 
 app.use(
